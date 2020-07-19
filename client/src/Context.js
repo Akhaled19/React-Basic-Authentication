b66aab4b431ec7f39ba7f452484e11a2c5a66692
Akhaled19 <56312360+Akhaled19@users.noreply.github.com>
2020-07-19 18:15:40 -0400

    add the Sign in function

--- client/src/Context.js
+++ client/src/Context.js
@@ -23,8 +23,9 @@ export class Provider extends Component {
   }
 
   
-  signIn = async () => {
-
+  signIn = async (username, password) => {
+    const user = await this.data.getUser(username, password);
+    return user 
   }
 
   signOut = () => {
