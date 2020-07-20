6d518c795014d4635e342d504a9ac60d6c4dc585
Akhaled19 <56312360+Akhaled19@users.noreply.github.com>
2020-07-20 17:28:28 -0400

    add o	Conditionally Render the Header Nav logic

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,15 +3,23 @@ import { Link } from 'react-router-dom';
 
 export default class Header extends React.PureComponent {
   render() {
+    const {context} = this.props;
+    const authUser = context.authenticatedUser;
     return (
       <div className="header">
         <div className="bounds">
           <h1 className="header--logo">MyAuth</h1>
           <nav>
-            <React.Fragment>
-              <Link className="signup" to="/signup">Sign Up</Link>
-              <Link className="signin" to="/signin">Sign In</Link>
-            </React.Fragment>
+            {authUser ? 
+              <React.Fragment>
+                <span>Welcome, {authUser.name}!</span>
+                <link to="/signout">Sign Out</link>
+              </React.Fragment>
+            : <React.Fragment>
+              <link className="signup" to="/signup">Sign Up</link> 
+              <link className="signin" to="/signin">Sign In</link>
+            </React.Fragment> 
+            }
           </nav>
         </div>
       </div>
